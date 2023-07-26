62567e089ca20ed270717507e447538af1104e3d
Egor Sorokin <egor.v.s@mail.ru>
2023-07-26 18:21:33 +0300

    Homework3 added

--- src/main/java/Homework3/LinkedList.java
+++ src/main/java/Homework3/LinkedList.java
@@ -1,2 +1,50 @@
-package Homework3;public class LinkedList {
+package Homework3;
+
+public class LinkedList {
+    private Node head;
+    private Node tail;
+
+    public void add(int value) {
+        Node node = new Node();
+        node.value = value;
+
+        if (head == null) {
+            head = node;
+        } else {
+            tail.next = node;
+        }
+        tail = node;
+    }
+
+    public void revert() {
+        if (head != null && head.next != null) {
+            tail = head;
+            Node current = head.next;
+            head.next = null;
+
+            while (current != null) {
+                Node next = current.next;
+                current.next = head;
+                head = current;
+                current = next;
+            }
+        }
+    }
+
+    public void print() {
+        Node current = head;
+        System.out.print("[ ");
+
+        while(current != null) {
+            System.out.print(current.value + " ");
+            current = current.next;
+        }
+
+        System.out.println("]");
+    }
+
+    private static class Node {
+        int value;
+        Node next;
+    }
 }
--- src/main/java/Homework3/Main.java
+++ src/main/java/Homework3/Main.java
@@ -1,2 +1,23 @@
-package Homework3;public class Main {
+package Homework3;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        for (int i = 0; i < 20; i++) {
+            list.add(new Random().nextInt(20));
+        }
+
+        System.out.print("Исходный список => ");
+        list.print();
+
+        System.out.println();
+
+        list.revert();
+        System.out.print("Развернутый список => ");
+        list.print();
+    }
 }
+
