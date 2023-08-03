d90aa9b50e042ef12045363da8495a84f0a1ce49
Egor Sorokin <egor.v.s@mail.ru>
2023-08-03 11:55:35 +0300

    Homework4 added

---
+++ src/main/java/Homework4/BinaryTree.java
@@ -0,0 +1,18 @@
+package Homework4;
+
+public class HashTree {
+    private class Node{
+        T value;
+        Color color;
+        Node left;
+        Node right;
+
+        Node() {
+            color = Color.red;
+        }
+    }
+
+    enum Color {red, black}
+
+public void print() {}
+}
